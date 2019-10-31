a6de04e04346233393dd3355c06d26d33157643e
Issa Khoury <i_kh@icloud.com>
2019-10-31 23:30:10 +0100

    Allow player to split

--- src/main/java/com/github/ikhoury/twentyone/player/Player.java
+++ src/main/java/com/github/ikhoury/twentyone/player/Player.java
@@ -2,37 +2,41 @@ package com.github.ikhoury.twentyone.player;
 
 import com.github.ikhoury.twentyone.deck.Card;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static com.github.ikhoury.twentyone.Constants.BUST_THRESHOLD;
+import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
 
 public class Player {
 
     private final String name;
-    private List<Card> hand;
-    private int points;
+    private List<CardHit> hits;
     private boolean stand;
 
     public Player(String name) {
         this.name = name;
-        this.hand = new ArrayList<>();
+        this.hits = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public List<Card> getHand() {
-        return hand;
+    public List<Card> getCards() {
+        return hits.stream()
+                .map(CardHit::getCard)
+                .collect(toList());
     }
 
     public int getPoints() {
-        return points;
+        return hits.stream()
+                .mapToInt(CardHit::getPoints)
+                .sum();
     }
 
     public boolean isBust() {
-        return points > BUST_THRESHOLD;
+        return getPoints() > BUST_THRESHOLD;
     }
 
     public boolean isStand() {
@@ -43,12 +47,77 @@ public class Player {
         return !isBust() && !isStand();
     }
 
+    public boolean canSplit() {
+        Set<Card> seenCards = new HashSet<>(hits.size());
+
+        for (Card currentCard : getCards()) {
+            if (seenCards.contains(currentCard)) {
+                return true;
+            }
+
+            seenCards.add(currentCard);
+        }
+
+        return false;
+    }
+
+    public boolean canSplitOn(Card card) {
+        Set<Card> seenCards = new HashSet<>(hits.size());
+
+        for (Card currentCard : getCards()) {
+            if (seenCards.contains(currentCard) && currentCard == card) {
+                return true;
+            }
+
+            seenCards.add(currentCard);
+        }
+
+        return false;
+    }
+
     public void hit(Card card, int points) {
-        this.hand.add(card);
-        this.points += points;
+        this.hits.add(new CardHit(card, points));
     }
 
     public void stand() {
         this.stand = true;
+    }
+
+    public void split(Card card) {
+        if (!canSplitOn(card)) {
+            throw new IllegalArgumentException(format("Card %s is not a candidate for splitting", card.name()));
+        }
+
+        int removed = 0;
+        Iterator<CardHit> handIterator = hits.iterator();
+        while (handIterator.hasNext()) {
+            if (removed == 2) {
+                break;
+            }
+
+            if (handIterator.next().getCard() == card) {
+                handIterator.remove();
+                removed++;
+            }
+        }
+    }
+
+    private static class CardHit {
+
+        private Card card;
+        private int points;
+
+        CardHit(Card card, int points) {
+            this.card = card;
+            this.points = points;
+        }
+
+        Card getCard() {
+            return card;
+        }
+
+        int getPoints() {
+            return points;
+        }
     }
 }
--- src/test/java/com/github/ikhoury/twentyone/player/PlayerTest.java
+++ src/test/java/com/github/ikhoury/twentyone/player/PlayerTest.java
@@ -13,6 +13,7 @@ public class PlayerTest {
     private static final String PLAYER_NAME = "John Doe";
     private static final int INITIAL_PLAYER_POINTS = 0;
     private static final int BUST_POINTS = BUST_THRESHOLD + 1;
+    private static final Card ACE_CARD = Card.ACE;
 
     private Player player;
 
@@ -24,18 +25,17 @@ public class PlayerTest {
     @Test
     public void createsNewPlayer() {
         assertThat(player.getName(), equalTo(PLAYER_NAME));
-        assertThat(player.getHand(), is(empty()));
+        assertThat(player.getCards(), is(empty()));
         assertThat(player.getPoints(), equalTo(INITIAL_PLAYER_POINTS));
     }
 
     @Test
     public void hitAddsCardAndPoints() {
-        Card card = Card.ACE;
         int points = 8;
 
-        player.hit(card, points);
+        player.hit(ACE_CARD, points);
 
-        assertThat(player.getHand(), hasItem(card));
+        assertThat(player.getCards(), hasItem(ACE_CARD));
         assertThat(player.getPoints(), equalTo(INITIAL_PLAYER_POINTS + points));
     }
 
@@ -48,7 +48,7 @@ public class PlayerTest {
 
     @Test
     public void bustsWhenOverLimit() {
-        player.hit(Card.ACE, BUST_POINTS);
+        player.hit(ACE_CARD, BUST_POINTS);
 
         assertThat(player.isBust(), is(true));
     }
@@ -60,7 +60,7 @@ public class PlayerTest {
 
     @Test
     public void cannotHitIfBust() {
-        player.hit(Card.ACE, BUST_POINTS);
+        player.hit(ACE_CARD, BUST_POINTS);
 
         assertThat(player.canHit(), is(false));
     }
@@ -70,5 +70,48 @@ public class PlayerTest {
         player.stand();
 
         assertThat(player.canHit(), is(false));
+    }
+
+    @Test
+    public void canSplitIfHasDuplicateCards() {
+        player.hit(ACE_CARD, 1);
+        player.hit(ACE_CARD, 1);
+
+        assertThat(player.canSplit(), is(true));
+        assertThat(player.canSplitOn(ACE_CARD), is(true));
+    }
+
+    @Test
+    public void cannotSplitIfDoesNotHaveDuplicateCards() {
+        player.hit(ACE_CARD, 1);
+
+        assertThat(player.canSplit(), is(false));
+        assertThat(player.canSplitOn(ACE_CARD), is(false));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void splitOnNonDuplicateCardThrowsException() {
+        player.hit(ACE_CARD, 1);
+        player.hit(ACE_CARD, 1);
+        player.hit(Card.EIGHT, 1);
+
+        player.split(Card.EIGHT);
+    }
+
+    @Test
+    public void splitOnDuplicateCardRemovesTwoCards() {
+        int duplicateCardPoints = 3;
+        Card otherCard = Card.EIGHT;
+        player.hit(ACE_CARD, duplicateCardPoints);
+        player.hit(ACE_CARD, duplicateCardPoints);
+        player.hit(ACE_CARD, duplicateCardPoints);
+        player.hit(otherCard, 5);
+        int previousPoints = player.getPoints();
+
+        player.split(ACE_CARD);
+
+        int expectedPoints = previousPoints - 2 * duplicateCardPoints;
+        assertThat(player.getPoints(), equalTo(expectedPoints));
+        assertThat(player.getCards(), containsInAnyOrder(ACE_CARD, otherCard));
     }
 }
