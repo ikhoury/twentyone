e5eddd85a507d5d1786302c63cc8dce5bfea7152
Issa Khoury <i_kh@icloud.com>
2019-10-31 22:55:52 +0100

    Fix constant

--- src/main/java/com/github/ikhoury/twentyone/Constants.java
+++ src/main/java/com/github/ikhoury/twentyone/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final int MAX_PLAYERS_PER_GAME = 3;
     public static final int BUST_THRESHOLD = 21;
-    public static final int BANK_STAND_THRESHOLD = 16;
+    public static final int BANK_STAND_THRESHOLD = 17;
 
     private Constants() {
 
