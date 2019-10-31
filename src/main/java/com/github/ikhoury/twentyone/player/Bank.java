f52d753e1eedeb137883e98301103b783578ee02
Issa Khoury <i_kh@icloud.com>
2019-10-31 22:42:47 +0100

    Bank hits and stands based on threshold

--- src/main/java/com/github/ikhoury/twentyone/player/Bank.java
+++ src/main/java/com/github/ikhoury/twentyone/player/Bank.java
@@ -1,10 +1,22 @@
 package com.github.ikhoury.twentyone.player;
 
+import static com.github.ikhoury.twentyone.Constants.BANK_STAND_THRESHOLD;
+
 public class Bank extends Player {
 
     private static final String BANK_NAME = "The Bank";
 
     public Bank() {
         super(BANK_NAME);
+    }
+
+    @Override
+    public boolean canHit() {
+        return super.canHit() && getPoints() < BANK_STAND_THRESHOLD;
+    }
+
+    @Override
+    public boolean isStand() {
+        return super.isStand() || getPoints() >= BANK_STAND_THRESHOLD;
     }
 }
---
+++ src/test/java/com/github/ikhoury/twentyone/player/BankTest.java
@@ -0,0 +1,31 @@
+package com.github.ikhoury.twentyone.player;
+
+import com.github.ikhoury.twentyone.deck.Card;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.github.ikhoury.twentyone.Constants.BANK_STAND_THRESHOLD;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class BankTest {
+
+    private Bank bank;
+
+    @Before
+    public void setUp() {
+        bank = new Bank();
+    }
+
+    @Test
+    public void canHitIfUnderThreshold() {
+        bank.hit(Card.ACE, BANK_STAND_THRESHOLD - 1);
+        assertThat(bank.canHit(), is(true));
+    }
+
+    @Test
+    public void standsIfOverThreshold() {
+        bank.hit(Card.ACE, BANK_STAND_THRESHOLD + 1);
+        assertThat(bank.isStand(), is(true));
+    }
+}
