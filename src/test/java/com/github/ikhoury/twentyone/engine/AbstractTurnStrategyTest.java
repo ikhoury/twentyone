f289424c2452f824d2f70796262fb53c20d3e56e
Issa Khoury <i_kh@icloud.com>
2019-12-03 22:41:03 +0100

    Create PlayerTurnStrategy that extracts player related logic from TurnEngine

---
+++ src/test/java/com/github/ikhoury/twentyone/engine/AbstractTurnStrategyTest.java
@@ -0,0 +1,11 @@
+package com.github.ikhoury.twentyone.engine;
+
+import com.github.ikhoury.twentyone.deck.Card;
+
+public abstract class AbstractTurnStrategyTest {
+
+    static final Card NEXT_CARD = Card.EIGHT;
+    static final Card CARD_WITH_MORE_THAN_ONE_POINTS = Card.ACE;
+    static final int CHOSEN_POINTS = 5;
+    static final int TURNS_TO_PLAY_AFTER_SPLIT = 2;
+}
